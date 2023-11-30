7f3b7265e548578850d24b651630356c40382f18
Rocío <Roco@Rociosnowhite>
2023-11-30 12:12:10 +0100

    cambio gatos

--- src/entornos/Clase3_3.java
+++ src/entornos/Clase3_3.java
@@ -5,6 +5,7 @@ public class Clase3_3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Practica 3_3 Rocío");
+		System.out.println("Me gusta los gatos");
 	}
 
 }
