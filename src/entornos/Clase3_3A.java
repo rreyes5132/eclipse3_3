c23035f47f694b6dac3adcb44c4423275bec6144
rreyes5132 <rreyes5132@ceuandalucia.es>
2023-12-01 09:15:45 +0100

    Cambios canela

--- src/entornos/Clase3_3A.java
+++ src/entornos/Clase3_3A.java
@@ -4,7 +4,7 @@ public class Clase3_3A {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Me gusta los gatos");
+		System.out.println("Canela en rama");
 	}
 
 }
